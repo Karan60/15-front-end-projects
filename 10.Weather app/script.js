02caec0b7162f2218f015e61b690c555d5ee2834
KARAN NATH <112185032+Karan60@users.noreply.github.com>
2024-02-07 20:17:48 +0530

    done

--- 10.Weather app/script.js
+++ 10.Weather app/script.js
@@ -7,14 +7,47 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }else{
+
+        var data = await response.json();
+
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    
+        if(data.weather[0].main =="Clouds"){
+            weatherIcon.src = "images/clouds.png";
+        }
+        else if(data.weather[0].main =="Clear"){
+            weatherIcon.src = "images/clear.png";
+        }
+        else if(data.weather[0].main =="Rain"){
+            weatherIcon.src = "images/rain.png";
+        }
+        else if(data.weather[0].main =="Drizzle"){
+            weatherIcon.src = "images/drizzle.png";
+        }
+        else if(data.weather[0].main =="Mist"){
+            weatherIcon.src = "images/mist.png";
+        }
+        document.querySelector(".weather").style.display ="block";
+        document.querySelector(".error").style.display ="none";
+
+    }
+
     var data = await response.json();
 
-    console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.main.speed + " km/h";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
     if(data.weather[0].main =="Clouds"){
         weatherIcon.src = "images/clouds.png";
